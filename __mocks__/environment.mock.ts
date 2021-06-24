2d3e86265a8b6aa3a7d01a1cbd48d0852f77d781
ASIM YILDIZ <info@asimyildiz.com>
2021-06-24 23:52:56 +0300

    [master] use fake environment variables for tests (CodePipeline)

---
+++ __mocks__/environment.mock.ts
@@ -0,0 +1,6 @@
+process.env.PORT = '8181';
+process.env.DATABASE_DEFAULT = 'mongodb+srv://@';
+process.env.DATABASE_URL = 'fake.mongodb.net';
+process.env.DATABASE_USER = 'fakeservice';
+process.env.DATABASE_PASS = 'xxAAbbCC';
+process.env.DATABASE_NAME = 'fakedb';
